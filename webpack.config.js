49b9e5db5608452131093d5575e2744be5d66991
dkunlun <519395527@qq.com>
2017-09-22 18:36:51 +0800

    修改运行环境基于webpack 改造成es6模式

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+
+    }
+}

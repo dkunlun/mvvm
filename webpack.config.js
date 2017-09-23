10ab4de354799081d0544a1c87b12c0f087768ff
dkunlun <519395527@QQ.COM>
2017-09-23 23:52:13 +0800

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'index.html',
             inject: true
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
+    devtool: 'inline-source-map',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
     },
     module: {
 
